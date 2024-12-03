15e76910045754dc8c3de346c12d4a2013bcbd86
ArMot <Arman.Motlagh@protonmail.com>
2024-12-03 13:09:25 +0330

    REPL for commands with no mechanism except ctrl+c to exit!

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    # Wait for user input
-    request = input()
-    command = request.split()[0]
 
-    sys.stderr.write(f"{command}: not found\n")
+    while True:
+        sys.stdout.write("$ ")
+        # Wait for user input
+        request = input()
+        command = request.split()[0]
+
+        sys.stderr.write(f"{command}: not found\n")
 
 
 if __name__ == "__main__":
