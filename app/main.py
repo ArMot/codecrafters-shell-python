60cd2bd832336b33dc24e93125ef2636dbc8c401
ArMot <Arman.Motlagh@protonmail.com>
2024-12-03 13:29:12 +0330

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Optional
 
 
 def main():
@@ -11,12 +12,24 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         request = input()
-        command = request.split()[0]
+        request = request.split()
+        command = request[0]
+        args = request[1:] if len(request) > 1 else []
 
         if command == EXIT:
             continue
+        elif command == "echo":
+            echo(args)
 
-        sys.stderr.write(f"{command}: not found\n")
+        else:
+            sys.stderr.write(f"{command}: not found\n")
+
+
+def echo(args: list):
+    if not args:
+        return
+    args_str = " ".join(str(arg) for arg in args)
+    sys.stdout.write(f"{args_str}\n")
 
 
 if __name__ == "__main__":
